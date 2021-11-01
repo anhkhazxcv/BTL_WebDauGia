3b2333b44f71b4d22b2a2b58c0fb2cb04f90008a
buikien1601 <66303699+buikien1601@users.noreply.github.com>
2021-11-01 21:27:07 +0700

    Update index.php

--- Đấu Giá/index.php
+++ Đấu Giá/index.php
@@ -1,61 +1,55 @@
 <?php
     include('header.php');
-    // $result = mysqli_query($conn,"SELECT * FROM cars");
-    // if(mysqli_num_rows($result) > 0){
-    //     $car = mysqli_fetch_all($result);
-    // }else echo 'Không đổ ra dữ liệu';
+    $result = mysqli_query($conn,"SELECT * FROM quanlysp");
+    if(mysqli_num_rows($result) > 0){
+        $quanlysp = mysqli_fetch_all($result);
+    }else echo 'Không đổ ra dữ liệu';
 ?> 
-
-<div class="panel-body mt-3">
+ <div class="panel-body mt-3">
           
-  <table class="table table-bordered">
-    <thead>
-      <tr>
-      <th>Mã phương tiện</th>
-			<th>Biển số xe</th>
-			<th>Model</th>
-	   	<th>Năm sản xuất</th>
-      <th>Kiểu oto</th>
-      <th>Giá cho thuê theo ngày</th>
-      <th>Giá cho thuê theo tuần</th>
-      <th>Trạng thái</th>
-			<th width="60px">Sửa</th>
-			<th width="60px">Xóa</th>
-      <th width="60px">Xem chi tiết</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>John</td>
-        <td>Doe</td>
-        <td>john@example.com</td>
-        <td>John</td>
-        <td>Doe</td>
-        <td>john@example.com</td>
-        <td>John</td>
-        <td>Doe</td>
-        <td><button class="btn btn-warning" >Edit</button></td>
-        <td><button class="btn btn-danger">Delete</button></td>
-        <td><button class="btn btn-warning">Details</button></td>
+          <table class="table table-bordered">
+            <thead>
+              <tr>
+              <th>Mã sản phẩm</th>
+              <th>Ảnh sản phẩm</th>
+              <th>Tên Sản phẩm</th>
+               <th>Mô tả sản phẩm</th>
+              <th>Giá khởi điểm đấu giá</th>
+              <th>Ngày đấu giá</th>
+              <th width="60px">Xem chi tiết</th>
+              <th width="60px">Sửa</th>
+              <th width="60px">Xóa</th>
+              </tr>
+            </thead>
+            <tbody>
+            <?php
+                foreach($quanlysp as $i){
+                    ?>
+              <tr>
+                  <th><?php echo $i[0]; ?></th>
+                  <th><?php echo $i[1]; ?></th>
+                  <th><?php echo $i[2]; ?></th>
+                  <th><?php echo $i[3]; ?></th>
+                  <th><?php echo $i[4]; ?></th>
+                  <th><?php echo $i[5]; ?></th>
+                
+                   <th><a href="details.php?id=<?php echo $i[0];?>"><i class="fas fa-eye"></i></a></th>
+                    <th><a href="edit.php?id=<?php echo $i[0];?>"><i class="far fa-edit"></i></a></th>
+                    <th><a href="delete.php?id=<?php echo $i[0];?>"><i class="far fa-trash-alt"></i></a></th>
+                
+              </tr>
+              <?php
+                        }
+                    ?>
+            </tbody>
         
-      </tr>
-      <tr>
-        <td>Mary</td>
-        <td>Moe</td>
-        <td>mary@example.com</td>
-      </tr>
-      <tr>
-        <td>July</td>
-        <td>Dooley</td>
-        <td>july@example.com</td>
-      </tr>
-    </tbody>
-  </table>
-</div>
+          </table>
+        </div>
 
-
-
-
+        
+    <form method="POST" action="create.php">
+        <button type="submit" class="btn btn-primary">Thêm sản phẩm</button>
+    </form>
 
 <?php
     include('footer.php');
