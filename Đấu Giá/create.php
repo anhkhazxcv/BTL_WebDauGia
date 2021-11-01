5f9cbac7de2eabf562c53b70936b10e05b2f7137
buikien1601 <66303699+buikien1601@users.noreply.github.com>
2021-11-01 21:28:42 +0700

    Update create.php

--- Đấu Giá/create.php
+++ Đấu Giá/create.php
@@ -1,12 +1,64 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+<?php
+    include('header.php');
+    $result = mysqli_query($conn,"SELECT * FROM quanlysp");
+    if(mysqli_num_rows($result) > 0){
+        $quanlysp = mysqli_fetch_all($result);
+    }else echo 'Không đổ ra dữ liệu';
+
+    if(isset($_POST['save'])){
+        $sp_ma = $_POST['sp_ma'];
+        $sp_anh = $_POST['sp_anh'];
+        $sp_ten = $_POST['sp_ten'];
+        $sp_mota = $_POST['sp_mota'];
+        $sp_gia = $_POST['sp_gia'];
+        $sp_ngay = $_POST['sp_ngay'];
+        
+
+        mysqli_query($conn, "INSERT INTO quanlysp (sp_ma, sp_anh, sp_ten, sp_mota, sp_gia, sp_ngay) VALUES ('$sp_ma','$sp_anh','$sp_ten','$sp_mota','$sp_gia','$sp_ngay') ");
+        header("location:   index.php");
+    }
     
-</body>
-</html>
+?>
+<div class="container">
+    <form method="POST">
+        <div class="form-group">
+            <label>Mã Sản Phẩm:</label>
+            <input type="number" class="form-control" name="sp_ma" >
+        </div>
+        <div class="form-group">
+            <label>Ảnh sản phẩm:</label>
+            <input type="text" class="form-control" name="sp_anh" >
+        </div>
+        <div class="form-group">
+            <label>Tên sản phẩm:</label>
+            <input type="text" class="form-control" name="sp_ten" >
+        </div>
+        <div class="form-group">
+            <label>Mô tả sản phẩm:</label>
+            <input type="text" class="form-control" name="sp_mota" >
+        </div>
+
+        <div class="form-group">
+            <label>Giá khởi điểm đấu giá:</label>
+            <input type="number" class="form-control" name="sp_gia" >
+        </div>
+
+        <div class="form-group">
+				  <label for="birthday">Ngày đấu giá:</label>
+				  <input type="date" class="form-control" id="year" name=" sp_ngay" >
+		</div>
+
+
+        <!-- <div class="form-group">
+            <label>Drate</label>
+            <input type="datetime" class="form-control" name="sp_ngay" >
+        </div>
+        -->
+        <div class="form-group">
+            <button type="submit" class="btn btn-primary" name="save">Lưu sản phẩm</button>
+        </div>
+    </form>
+</div>
+<?php
+    include('footer.php');
+?>
