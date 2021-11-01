18322eb76bfb3f14aba5b1fdba28ad15e5d7f704
buikien1601 <66303699+buikien1601@users.noreply.github.com>
2021-11-01 21:29:21 +0700

    Update config.php

--- Đấu Giá/config.php
+++ Đấu Giá/config.php
@@ -1,5 +1,5 @@
 <?php
-    $conn = mysqli_connect('localhost', 'root', '', '');
+    $conn = mysqli_connect('localhost', 'root', '', 'web_daugia');
     if(!$conn){
         die("Kết nối thất bại" . mysqli_connect_error());
     } 
