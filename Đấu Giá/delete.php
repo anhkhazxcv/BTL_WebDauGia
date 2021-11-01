4fc57dc378c58a2bc9317042e1e2b7681e0321f1
buikien1601 <66303699+buikien1601@users.noreply.github.com>
2021-11-01 23:33:31 +0700

    Update delete.php

--- Đấu Giá/delete.php
+++ Đấu Giá/delete.php
@@ -1,12 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php
+    include('header.php');
+    if(isset($_GET['id'])){
+        $id = $_GET['id'];
+        mysqli_query($conn, "DELETE FROM quanlysp WHERE sp_ma='$id'");
+        header("location: index.php");
+    }
+?>
+
+<?php
+    include('footer.php');
+?>
