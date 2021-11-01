f8d0ffdb43537f0ca42ca6cace9e1202f8af2e28
buikien1601 <66303699+buikien1601@users.noreply.github.com>
2021-11-01 18:17:02 +0700

    Update index.php

--- Đấu Giá/index.php
+++ Đấu Giá/index.php
@@ -1,12 +1,62 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php
+    include('header.php');
+    // $result = mysqli_query($conn,"SELECT * FROM cars");
+    // if(mysqli_num_rows($result) > 0){
+    //     $car = mysqli_fetch_all($result);
+    // }else echo 'Không đổ ra dữ liệu';
+?> 
+
+<div class="panel-body mt-3">
+          
+  <table class="table table-bordered">
+    <thead>
+      <tr>
+      <th>Mã phương tiện</th>
+			<th>Biển số xe</th>
+			<th>Model</th>
+	   	<th>Năm sản xuất</th>
+      <th>Kiểu oto</th>
+      <th>Giá cho thuê theo ngày</th>
+      <th>Giá cho thuê theo tuần</th>
+      <th>Trạng thái</th>
+			<th width="60px">Sửa</th>
+			<th width="60px">Xóa</th>
+      <th width="60px">Xem chi tiết</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>John</td>
+        <td>Doe</td>
+        <td>john@example.com</td>
+        <td>John</td>
+        <td>Doe</td>
+        <td>john@example.com</td>
+        <td>John</td>
+        <td>Doe</td>
+        <td><button class="btn btn-warning" >Edit</button></td>
+        <td><button class="btn btn-danger">Delete</button></td>
+        <td><button class="btn btn-warning">Details</button></td>
+        
+      </tr>
+      <tr>
+        <td>Mary</td>
+        <td>Moe</td>
+        <td>mary@example.com</td>
+      </tr>
+      <tr>
+        <td>July</td>
+        <td>Dooley</td>
+        <td>july@example.com</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+
+
+
+
+
+<?php
+    include('footer.php');
+?>
