2618744347108408343c36b9539100169d58a589
anhkhazxcv <anhkhazxcv@gmail.com>
2021-11-02 08:32:55 +0700

    demo dau gia sp

---
+++ sell.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+include 'config.php';
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Bán Sản Phẩm</title>
+<style>
+.error {color: #FF0000;}
+</style>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body background="images/pic11.jpg">
+
+
+<div id="login">
+<?php
+include 'config.php';
