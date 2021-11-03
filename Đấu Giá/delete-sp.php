8fdca2366364172ce1e41329f4a01231329bb9af
buikien1601 <66303699+buikien1601@users.noreply.github.com>
2021-11-03 14:46:01 +0700

    Update and rename delete.php to delete-sp.php

--- Đấu Giá/delete-sp.php
+++ Đấu Giá/delete-sp.php
@@ -1,12 +1,12 @@
 <?php
-    include('header.php');
+    include('header-sp.php');
     if(isset($_GET['id'])){
         $id = $_GET['id'];
         mysqli_query($conn, "DELETE FROM quanlysp WHERE sp_ma='$id'");
-        header("location: index.php");
+        header("location: index-sp.php");
     }
 ?>
 
 <?php
-    include('footer.php');
+    include('footer-sp.php');
 ?>
