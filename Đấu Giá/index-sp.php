3ae50c0ef5e9e5e53ef54f6dff2d7663bb6b28ad
buikien1601 <66303699+buikien1601@users.noreply.github.com>
2021-11-03 14:43:22 +0700

    Update and rename index.php to index-sp.php

--- Đấu Giá/index-sp.php
+++ Đấu Giá/index-sp.php
@@ -1,5 +1,5 @@
 <?php
-    include('header.php');
+    include('header-sp.php');
     $result = mysqli_query($conn,"SELECT * FROM quanlysp");
     if(mysqli_num_rows($result) > 0){
         $quanlysp = mysqli_fetch_all($result);
@@ -11,11 +11,16 @@
             <thead>
               <tr>
               <th>Mã sản phẩm</th>
+              <th>Loại sản phẩm</th>
               <th>Ảnh sản phẩm</th>
               <th>Tên Sản phẩm</th>
-               <th>Mô tả sản phẩm</th>
+              <th>Mô tả sản phẩm</th>
               <th>Giá khởi điểm đấu giá</th>
-              <th>Ngày đấu giá</th>
+              <th>Ngày bắt đầu đấu giá</th>
+              <th>Ngày kết thúc đấu giá</th>
+              <th>Người bán sản phẩm</th>
+              <th>Trạng thái sản phẩm</th>
+              <th>Người mua sản phẩm</th>
               <th width="60px">Xem chi tiết</th>
               <th width="60px">Sửa</th>
               <th width="60px">Xóa</th>
@@ -32,10 +37,15 @@
                   <th><?php echo $i[3]; ?></th>
                   <th><?php echo $i[4]; ?></th>
                   <th><?php echo $i[5]; ?></th>
+                  <th><?php echo $i[6]; ?></th>
+                  <th><?php echo $i[7]; ?></th>
+                  <th><?php echo $i[8]; ?></th>
+                  <th><?php echo $i[9]; ?></th>
+                  <th><?php echo $i[10]; ?></th>
                 
-                   <th><a href="details.php?id=<?php echo $i[0];?>"><i class="fas fa-eye"></i></a></th>
-                    <th><a href="edit.php?id=<?php echo $i[0];?>"><i class="far fa-edit"></i></a></th>
-                    <th><a href="delete.php?id=<?php echo $i[0];?>"><i class="far fa-trash-alt"></i></a></th>
+                   <th><a href="details-sp.php?id=<?php echo $i[0];?>"><i class="fas fa-eye"></i></a></th>
+                    <th><a href="edit-sp.php?id=<?php echo $i[0];?>"><i class="far fa-edit"></i></a></th>
+                    <th><a href="delete-sp.php?id=<?php echo $i[0];?>"><i class="far fa-trash-alt"></i></a></th>
                 
               </tr>
               <?php
@@ -47,10 +57,10 @@
         </div>
 
         
-    <form method="POST" action="create.php">
+    <form method="POST" action="create-sp.php">
         <button type="submit" class="btn btn-primary">Thêm sản phẩm</button>
     </form>
 
 <?php
-    include('footer.php');
+    include('footer-sp.php');
 ?>
