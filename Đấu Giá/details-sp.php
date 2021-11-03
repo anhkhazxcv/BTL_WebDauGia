6c097d7e6872a8c0b18568ee1e2d28f4fc3099b4
buikien1601 <66303699+buikien1601@users.noreply.github.com>
2021-11-03 14:45:35 +0700

    Update and rename details.php to details-sp.php

--- Đấu Giá/details-sp.php
+++ Đấu Giá/details-sp.php
@@ -1,16 +1,21 @@
 <?php
-    include('header.php');
+    include('header-sp.php');
     if(isset($_GET['id'])){
         $id = $_GET['id'];
         $result = mysqli_query($conn, "SELECT * FROM quanlysp WHERE sp_ma= '$id'");
         if(mysqli_num_rows($result)==1){
             $quanlysp = mysqli_fetch_array($result);
             $sp_ma =  $quanlysp['sp_ma'];
+            $sp_loai = $quanlysp['sp_loai'];
             $sp_anh =  $quanlysp['sp_anh'];
             $sp_ten =  $quanlysp['sp_ten'];
             $sp_mota =  $quanlysp['sp_mota'];
             $sp_gia =  $quanlysp['sp_gia'];
-            $sp_ngay =  $quanlysp['sp_ngay'];
+            $sp_ngaybd =  $quanlysp['sp_ngaybd'];
+            $sp_ngaykt = $quanlysp['sp_ngaykt'];
+            $sp_nguoiban = $quanlysp['sp_nguoiban'];
+            $sp_trangthai = $quanlysp['sp_trangthai'];
+            $sp_nguoimua = $quanlysp['sp_nguoimua'];
             
         }
     }
@@ -19,6 +24,10 @@
         <div class="form-group">
             <label>Mã sản phẩm: </label> <br>
             <?php echo $id?>
+        </div>
+        <div class="form-group">
+            <label>Loại sản phẩm:</label><br>
+            <?php echo $sp_loai?>
         </div>
         <div class="form-group">
             <label>Ảnh sản phẩm: </label> <br>
@@ -39,14 +48,30 @@
         </div>
         <div class="form-group">
             <label>Ngày đấu giá: </label><br>
-            <?php echo $sp_ngay?>
+            <?php echo $sp_ngaybd?>
         </div>
+        <div class="form-group">
+			<label for="birthday">Ngày kết thúc đấu giá:</label>
+			<?php echo $sp_ngaykt?>
+		</div>
+        <div class="form-group">
+            <label>Người bán sản phẩm:</label><br>
+            <?php echo $sp_nguoiban?>
+        </div>
+        <div class="form-group">
+            <label>Trạng thái sản phẩm:</label><br>
+            <?php echo $sp_trangthai?>
+        </div>
+        <div class="form-group">
+            <label>Người mua sản phẩm:</label><br>
+            <?php echo $sp_nguoimua?>
+        </div> 
       
-        <form method="POST" action="index.php">
+        <form method="POST" action="index-sp.php">
         <button type="submit" class="btn btn-primary">Back</button>
     </form>
 </div>
 
 <?php
-    include('footer.php');
+    include('footer-sp.php');
 ?>
