80987163e95deae88cd96d3763e3eded8ced3fd0
buikien1601 <66303699+buikien1601@users.noreply.github.com>
2021-11-04 14:38:02 +0700

    Update delete-sp.php

--- Đấu Giá/delete-sp.php
+++ Đấu Giá/delete-sp.php
@@ -2,7 +2,7 @@
     include('header-sp.php');
     if(isset($_GET['id'])){
         $id = $_GET['id'];
-        mysqli_query($conn, "DELETE FROM quanlysp WHERE sp_ma='$id'");
+        mysqli_query($conn, "DELETE FROM product WHERE sp_id='$id'");
         header("location: index-sp.php");
     }
 ?>
